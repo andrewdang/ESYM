598f4f9e78dfbb63273a20393e81a2bae5b96f30
andrewdang <andrew@outlook.com>
2017-03-04 10:41:36 -0800

    add nav

--- app/assets/javascripts/components/navbar.js
+++ app/assets/javascripts/components/navbar.js
@@ -3,8 +3,28 @@ import React, { Component } from 'react'
 class NavBar extends Component {
   render() {
     return (
-      <nav>
-        <h2>Test</h2>
+      <nav className="nav has-shadow">
+        <div className="container">
+          <div className="nav-left">
+            <a className="nav-item">
+              <img src="http://bulma.io/images/bulma-logo.png" alt="logo" />
+            </a>
+            <a className="nav-item is-tab is-hidden-mobile is-active">Menu</a>
+          </div>
+          <span className="nav-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+          <div className="nav-right nav-menu">
+            <a className="nav-item is-tab">
+              <figure className="image is-16x16">
+                <img src="http://bulma.io/images/jgthms.png" />
+              </figure>
+            </a>
+            <a className="nav-item is-tab">Log out</a>
+          </div>
+        </div>
       </nav>
     )
   }
